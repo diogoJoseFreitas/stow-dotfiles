97085c5285d86a6f6bfff82e1ca5c04879853a4a
DiogoJoseFreitas <diogojosefreitas@proton.me>
2026-02-25 21:08:52 -0400

    feat: android emulator no rider

--- rider/.oh-my-zsh/custom/rider.zsh
+++ rider/.oh-my-zsh/custom/rider.zsh
@@ -1,4 +1,10 @@
 rider() {
   nohup /home/diogo/.local/share/JetBrains/Toolbox/apps/rider/bin/rider "$@" >/dev/null 2>&1 &
 }
+export JAVA_HOME="/usr/lib/jvm/java-17-openjdk-amd64"
+export PATH="$PATH:$JAVA_HOME/bin"
+# Android SDK
+export ANDROID_HOME="$HOME/Android/Sdk"
+export ANDROID_SDK_ROOT="$HOME/Android/Sdk"
+export PATH="$PATH:$ANDROID_HOME/platform-tools"
 
