103d3ed86e53ecc00dd1e292da7f57e06c6e4bcb
diogoJoseFreitas <diogoJoseFreitas@proton.me>
2026-03-31 23:31:42 -0400

    fixed identation

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -90,11 +90,7 @@ return {
   },
   {
     "nvim-lualine/lualine.nvim",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    opts = {
-      theme = "auto",
-    }
+    dependencies = { "nvim-tree/nvim-web-devicons", },
+    opts = { theme = "auto", }
   },
 }
