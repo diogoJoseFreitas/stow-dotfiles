817e02d2a818bcfd6911b2d51c8ff69b5d6c7fd6
diogoJoseFreitas <diogoJoseFreitas@proton.me>
2026-09-06 12:43:34 -0400

    change: tabstop

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,13 +1,13 @@
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
-vim.opt.shiftwidth=4
+vim.opt.shiftwidth=2
 
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
 
-vim.opt.shell = "/usr/bin/fish"
+vim.opt.shell = "/home/linuxbrew/.linuxbrew/bin/fish"
 
 -- Clipboard Definitions
 vim.opt.clipboard = 'unnamedplus'
